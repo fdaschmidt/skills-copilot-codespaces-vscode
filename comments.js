05cbd248d47558177673976bbe58bffd93db3760
Frédéric Schmidt <frederic.schmidt@outlook.com>
2024-05-01 18:35:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,4 @@
 // Create a web server
-// Create a web server that listens on port 3000 and serves the following responses:
-// - GET /comments - returns a list of comments
-// - POST /comments - create a new comment
-// - GET /comments/:id - return a single comment with the matching id
-// - PUT /comments/:id - update a comment with the matching id
-// - DELETE /comments/:id - delete a comment with the matching id
-// The comments are stored in a JSON file on the server. Use the fs module to read/write to this file.
-// You can use this array of comments to get started with:
-// const comments = [
-//   { id: 1, author: "user1", content: "content1" },
-//   { id: 2, author: "user2", content: "content2" },
-//   { id: 3, author: "user3", content: "content3" },
-//   { id: 4, author: "user4", content: "content4" },
-// ];
 
 // Path: comments.js
 const fs = require("fs");
